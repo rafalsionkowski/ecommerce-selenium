6fbb6100ffe1fa5be76c79966439166c0dec327c
rafal.sionkowski <rafal.sionkowski@gmail.com>
2022-07-20 20:42:54 +0200

    Change propertis for log4j2.properties

---
+++ src/main/java/utils/dbconnection/test.java
@@ -0,0 +1,18 @@
+package utils.dbconnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class test {
+
+//    public static void main(String[] args) throws SQLException {
+//        if (QueryExecutor.executeQuery
+//                ("INSERT user (user_name, user_password) VALUES ('jan_n', 'qwerty')")) {
+//            System.out.println("User Added");
+//        } else {
+//            System.out.println("User Added");
+//        }
+//
+//    }
+
+}
